2c3413fe722985b94835f34412c8a3b8812406ad
vitaliy <vitaliydoc.fish@gmail.comgit config --global user.email vitaliydoc.fish@gmail.com>
2019-08-11 22:42:23 +0300

    added  homework number four

--- task#4/main.js
+++ task#4/main.js
@@ -14,132 +14,87 @@
 //Решение:
 
 'use strict';
-
-//-----------------Variant 1 --------------------
-let operator;
-let argumentA = null;
-let argumentB = null;
-// let result = null;
-
-//1 Ask what user does want to do?
-
-do {
-   operator = prompt('Chouse operator add,sub,div,mult?', '');
-   operator = operator.toLowerCase();
-}while(operator != 'add' &&
-       operator != 'sub' &&
-       operator != 'div' &&
-       operator != 'mult' 
-)
-
-
-//2 Enter arguments ------------------------------
-do{argumentA = +prompt('Enter first operand!', '');
-}while(isNaN(argumentA) ||
-             argumentA <=0
-)
-
-do{argumentB = +prompt('Enter second operand!', '')
-}while(isNaN(argumentB) ||
-             argumentB <=0
-)
-
-
-  //-----------declared function------------------------
- function add(){
-   return  argumentA + argumentB;
-  };
  
- function sub(){
-   return argumentA - argumentB;
+//1 Спрашеваeм действие у пользователя
+
+ function getOperator() {
+  let operator = prompt('Chouse operator, add,sub,div,mult', '');
+      operator = operator.toLowerCase();
+  if(operator !== 'add' &&
+     operator !== 'sub' &&
+     operator !== 'div' &&
+     operator !== 'mult') {
+       alert('Enter right action!');
+       return getOperator()   
+   }
+    return operator;
  };
 
- function div(){
-   return argumentA / argumentB;
- };
 
- function mult(){
-   return argumentA * argumentB;
- };
+//2 Спрашеваем первый аргумент 
 
-
- //------------- Calculate----------------------------
-switch(operator){
-  case 'add': alert('Result' + ' = ' + add());
-  break;
-  case 'sub': alert('Result' + ' = ' + sub());
-  break;
-  case 'div': alert('Result' + ' = ' + div());
-  break;
-  case 'mult': alert('Result' + ' = ' + mult());
-  break;
+function getArgument1() {
+  let argument1 = +prompt('Enter first operand!', '');
+  if(isNaN(argument1) ||
+      argument1 <=0
+  ){
+    alert('Enter a number!!');
+    return getArgument1()
+  }
+  return argument1;
 };
 
+//3 Спрашеваем второй аргумент
 
-
-//----------------------------------------------------
-//--------------- Variant 2 ----------------------------
-
-/*
-let operator;
-let argumentsCount = 3;
-let argument = null;
-// let argumentB = null;
-// let result = null;
-
-
-
-const add = function() {
-  return argument + argument;
+function getArgument2() {
+  let argument2 = +prompt('Enter second operand!', '');
+  if(isNaN(argument2) ||
+      argument2 <=0
+  ){
+    alert('Enter a number!!');
+    return getArgument2()
+  }
+  return argument2;
 };
-const sub = function() {
-  return argument - argument;
-};
-const div = function() {
-  return argument / argument;
-};
-const mult = function() {
-  return argument * argument;
-};
+// console.log(getArgument2());
+
+// Выполняем действие и выводим результат
+
+function calculate(argument1, argument2, operator) {
+  let result ;
+  switch(operator){
+    case 'add': result = argument1 + argument2;break;
+    case 'sub': result = argument1 - argument2;break;
+    case 'div': result = argument1 / argument2;break;
+    case 'mult': result = argument1 * argument2;break;
+  }
+  return result;
+}
+
+  let operator  = getOperator();
+  let argument1 = getArgument1();
+  let argument2 = getArgument2();
+  let result    = calculate(argument1, argument2, operator);
+
+alert('Result' + ' = ' + result);
+
+  //----------------------------
 
 
 
-//1 Ask what user does want to do?
-
-do {
-   operator = prompt('Chouse operator add,sub,div,mult?', '');
-   operator = operator.toLowerCase();
-}while(operator != 'add' &&
-       operator != 'sub' &&
-       operator != 'div' &&
-       operator != 'mult' 
-);
 
 
-//2 Enter operands
-for(let i = 1;i < argumentsCount;i++) {
-do{
-  argument = +prompt(' Enter argument ' + i, '');
- }while(isNaN(argument) ||
-        argument <= 0
- )
-};
 
 
-switch(operator){
-  case 'add': alert('Result' + ' = ' + add());
-  break;
-  case 'sub': alert('Result' + ' = ' + sub());
-  break;
-  case 'div': alert('Result' + ' = ' + div());
-  break;
-  case 'mult': alert('Result' + ' = ' + mult());
-  break;
-};
 
-*/
 
-//--------------------------------------------------
+
+
+
+
+
+
+
 
 
 
